27e8aa7f647e029a707145c708e3f86518321fff
twins200202 <twins2002002@gmail.com>
2019-09-02 14:23:46 +0900

    캘린더 만들기 완료

--- src/Day.vue
+++ src/Day.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="day">
+    <div class="day" @click="openPopup">
         <div class="date">
             <span class="number">{{day.date.getDate()}}</span>
             <span class="month">({{day.date.getMonth() + 1}}월)</span>
         </div>
         <div class="list">
-            <div class="item" v-for="todo in day.list">{{todo.name}}</div>
+            <div class="item" v-for="(todo, idx) in day.list" @click.stop="openEditPopup(idx)">{{todo.name}}</div>
         </div>
     </div>
 </template>
@@ -17,6 +17,14 @@
         data(){
             return {
                 
+            }
+        },
+        methods:{
+            openPopup(){
+                this.$emit("open");
+            },
+            openEditPopup(idx){
+                this.$emit("edit", event, idx, this.day.idx);
             }
         }
     }
@@ -44,6 +52,14 @@
     .month {
         font-size:15px;
         color:#555;
+    }
+
+    .item {
+        background-color: #3af;
+        color:#fff;
+        border-radius: 0.25rem;
+        padding:0.2rem 0.25rem;
+        margin:4px;
     }
 
 </style>
